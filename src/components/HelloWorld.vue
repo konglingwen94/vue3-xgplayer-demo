cc5c27734adc8c8444af39d81436e9a587a6bfd4
konglingwen94 <794430774@qq.com>
2021-07-07 16:20:29 +0800

    初始化播放器

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="vs">xgplayer</div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import Player from "xgplayer";
+
+onMounted(() => {
+  const player = new Player({
+    id: "vs",
+    autoplay: false,
+    playbackRate: [0.5, 0.75, 1, 1.5, 2],
+    defaultPlaybackRate: 1.5,
+    volume: 0.3,
+    url: "//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4",
+    poster: "//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg",
+    playsinline: true,
+    height: 399,
+    // width: window.innerWidth
+  });
+});
+</script>
+
+<style scoped>
+ 
+</style>
